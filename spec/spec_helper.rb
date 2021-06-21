4140d8cdf42fbb6ec19078b4fe4aacb16f8b6b21
novikov.vasiliy <novikov@oneclick.life>
2021-06-21 11:15:36 +0300

    Fix github actions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,9 +5,6 @@ require 'omniauth'
 require 'omniauth-tiktok'
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = '.rspec_status'
-
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
